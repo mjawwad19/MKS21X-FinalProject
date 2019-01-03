23b8f5864afd09dafcf3709eb5834073bf99ca9d
Mohammad Jawwad <mjawwad@stuy.edu>
2019-01-03 17:16:27 -0500

    Added skeleton of graph class

--- Graph.java
+++ Graph.java
@@ -1,3 +1,18 @@
 public class Graph{
-  
+  char[][] grid;
+  Polynomial equation;
+  int startX = -40;
+  int startY = -40;
+  int endX = 40;
+  int endY = 40;
+  // creates empty graph grid (2D array) [helper]
+  private void helpGraph(int minX, int maxX, int minY, int maxY) {}
+  //finds center row and col and labels as axis
+  private void axis() {}
+  //graphs the equation given
+  public Graph() {}
+  //graphs the equation given and the bounds specified
+  public Graph (int minX, int maxX, int minY, int maxY) {}
+  //output for user to see
+  public String toString() {return "dummy";}
 }

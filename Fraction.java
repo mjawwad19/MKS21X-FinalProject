c0f1519f476d063c9f9ed17a7cb6a6a610640b43
phuang00 <phuang00@stuy.edu>
2019-01-03 11:01:59 -0500

    wrote part of constructor and started private helper

--- Fraction.java
+++ Fraction.java
@@ -1,3 +1,15 @@
 public class Fraction{
-  
+  private int numerator,denominator;
+
+  public Fraction(int num, int deno){
+    numerator = num;
+    denominator = deno;
+    simplify();
+  }
+
+  private void simplify(){
+    
+  }
+
+
 }

0a3185063cf4b2a952a9ce500cc83013dd3fbff9
Mohammad Jawwad <mjawwad@stuy.edu>
2019-01-04 16:44:50 -0500

    test

--- Monomial.java
+++ Monomial.java
@@ -27,12 +27,54 @@ public class Monomial{
     return "" + getCoef() + getVar() + "^" + "(" + getDeg() + ")";
   }
 
-  public static void main(String[] args) {
-    Monomial a = new Monomial(new Fraction(4, 1), 'x', 2);
-    Monomial b = new Monomial(new Fraction(0, 4), 'w', 5);
-    Monomial c = new Monomial(new Fraction(3,5), 'q', 0);
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
+  public void setCoef(int co) {
+    coef = co;
   }
+
+  public void setVar(char v) {
+    var = v;
+  }
+
+  public void setDeg(int deg) {
+    degree = deg;
+  }
+
+  public boolean likeTerms(Monomial other) {
+    if (getVar() == other.getVar()) return true;
+    return false;
+  }
+
+  public boolean likeDeg(Monomial other) {
+    if (getDeg() == other.getDeg()) return true;
+    return false;
+  }
+  /*Below is a WIP for add(Monomial) because of certain issues
+  1) if you add two monomials together and they aren't compatible, then they
+  would create a Polynomial.
+  In that case shouldn't this be moved to the Polynomial class?
+  2) A proposed solution to this: Make this add a boolean that
+  modifies the Monomial calling the function if possible */
+  public Polynomial add(Monomial other) {
+  //  Polynomial end = new Polynomial();
+    Monomial out = (new Fraction(0, 1), 'x', 0); //default
+    if (likeTerms && likeDeg) {
+      out.setCoef(getCoef() + other.getCoef());
+      out.setVar(getVar());
+      out.setDeg(getDeg() + other.getDeg());
+    //  end.add(out);
+    }
+    /*else {
+      end.add(this);
+      end.add(other);
+    }*/
+  }
+
+  public static void main(String[] args) {
+   Monomial a = new Monomial(new Fraction(4, 1), 'x', 2);
+   Monomial b = new Monomial(new Fraction(0, 4), 'w', 5);
+   Monomial c = new Monomial(new Fraction(3,5), 'q', 0);
+   System.out.println(a);
+   System.out.println(b);
+   System.out.println(c);
+ }
 }

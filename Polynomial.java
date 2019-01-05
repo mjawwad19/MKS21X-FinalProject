fabbf13268194887de9a934442b5c5dc58fa39a2
phuang00 <phuang00@stuy.edu>
2019-01-04 23:03:31 -0500

    wrote solveQuad method and fixed add and subtract methods

--- Polynomial.java
+++ Polynomial.java
@@ -13,19 +13,19 @@ public class Polynomial{
     }
   }
 
-  public Polynomial add(Monomial other){
+  public void add(Monomial other){
+    boolean added = false;
     for (int i = 0; i < monos.size(); i++){
       if (monos.get(i).likeTerms(other)){
         monos.get(i).setCoef(monos.get(i).getCoef().add(other.getCoef()));
-        return new Polynomial(monos);
+        added = true;
       }
     }
-    monos.add(other);
-    return new Polynomial(monos);
+    if (!added) monos.add(other);
   }
 
-  public Polynomial subtract(Monomial other){
-    return add(other.multiply(new Monomial(new Fraction(-1, 1), other.getVar(), 0)));
+  public void subtract(Monomial other){
+    add(other.multiply(new Monomial(new Fraction(-1, 1), other.getVar(), 0)));
   }
 
   public String toString(){
@@ -39,12 +39,36 @@ public class Polynomial{
     return ans;
   }
 
+  public Fraction[] solveQuad(){
+    Fraction a = new Fraction(0, 1);
+    Fraction b = new Fraction(0, 1);
+    Fraction c = new Fraction(0, 1);
+    for (Monomial term: monos){
+      if (term.getDeg() == 2) a = term.getCoef();
+      if (term.getDeg() == 1) b = term.getCoef();
+      if (term.getDeg() == 0) c = term.getCoef();
+    }
+    Fraction[] ans = new Fraction[2];
+    Fraction discrim = b.power(2).subtract(a.multiply(c).multiply(new Fraction(4, 1)));
+    if (Double.parseDouble(discrim.toString()) < 0) throw new IllegalArgumentException("There are no real roots, the discriminant is " + discrim);
+    ans[0] = ((new Fraction(0, 1)).subtract(b).add(discrim.power(0.5))).divide(a.multiply(new Fraction(2,1)));
+    ans[1] = ((new Fraction(0, 1)).subtract(b).subtract(discrim.power(0.5))).divide(a.multiply(new Fraction(2,1)));
+    return ans;
+  }
+
   public static void main(String[] args) {
     Polynomial a = new Polynomial();
-    a = a.add(new Monomial(new Fraction(4, 1), 'x', 2));
-    a = a.add(new Monomial(new Fraction(-6, 1), 'x', 5));
-    a = a.add(new Monomial(new Fraction(9, 1), 'x', 5));
-    a = a.subtract(new Monomial(new Fraction(3, 1), 'x', 5));
+    a.add(new Monomial(new Fraction(4, 1), 'x', 2));
+    a.add(new Monomial(new Fraction(-6, 1), 'x', 5));
+    a.add(new Monomial(new Fraction(9, 1), 'x', 5));
+    a.subtract(new Monomial(new Fraction(3, 1), 'x', 5));
     System.out.println(a);
+    Polynomial b = new Polynomial();
+    b.add(new Monomial(new Fraction(1, 1), 'x', 2));
+    b.add(new Monomial(new Fraction(-1, 1), 'x', 1));
+    b.add(new Monomial(new Fraction(-6, 1), 'x', 0));
+    System.out.println(b.solveQuad()[0]);
+    System.out.println(b.solveQuad()[1]);
+    System.out.println(b);
   }
 }

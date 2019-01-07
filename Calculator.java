890d4e51d71b49e621689613cd207378a3a689f0
phuang00 <phuang00@stuy.edu>
2019-01-07 11:00:58 -0500

    wrote four function in main

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,39 @@
+import java.util.*;
 public class Calculator{
-  
+  public static void main(String[] args) {
+    String[] temp = args[0].split(" ");
+    ArrayList<String> input = new ArrayList<>();
+    for (String arg: temp){
+      input.add(arg);
+    }
+    for (int i = 0; i < input.size(); i++){
+      if (input.get(i).equals("*")){
+        input.set(i, "" + Double.parseDouble(input.get(i - 1)) * Double.parseDouble(input.get(i + 1)));
+        input.remove(i - 1);
+        input.remove(i);
+        i -= 1;
+      }
+      else if (input.get(i).equals("/")){
+        input.set(i, "" + Double.parseDouble(input.get(i - 1)) / Double.parseDouble(input.get(i + 1)));
+        input.remove(i - 1);
+        input.remove(i);
+        i -= 1;
+      }
+    }
+    for (int i = 0; i < input.size(); i++){
+      if (input.get(i).equals("+")){
+        input.set(i, "" + (Double.parseDouble(input.get(i - 1)) + Double.parseDouble(input.get(i + 1))));
+        input.remove(i - 1);
+        input.remove(i);
+        i -= 1;
+      }
+      else if (input.get(i).equals("-")){
+        input.set(i, "" + (Double.parseDouble(input.get(i - 1)) - Double.parseDouble(input.get(i + 1))));
+        input.remove(i - 1);
+        input.remove(i);
+        i -= 1;
+      }
+    }
+    System.out.println(input.get(0));
+  }
 }

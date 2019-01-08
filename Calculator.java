8488498787560c07b8c5b3976259617007febae3
phuang00 <phuang00@stuy.edu>
2019-01-07 23:39:46 -0500

    rearranged code so that adding, subtracting, dividing, multiplying, and raising a number to a certain power is in a new solve method and dealt with parenthesis in the main

--- Calculator.java
+++ Calculator.java
@@ -1,23 +1,26 @@
 import java.util.*;
 public class Calculator{
-  public static void main(String[] args) {
-    String[] temp = args[0].split(" ");
-    ArrayList<String> input = new ArrayList<>();
-    for (String arg: temp){
-      input.add(arg);
+  public static double solve(List<String> input){
+    for (int i = 0; i < input.size(); i++){
+      if (input.get(i).equals("**")){
+        input.set(i, "" + Math.pow(Double.parseDouble(input.get(i - 1)),Double.parseDouble(input.get(i + 1))));
+        input.remove(i - 1);
+        input.remove(i);
+        i--;
+      }
     }
     for (int i = 0; i < input.size(); i++){
       if (input.get(i).equals("*")){
         input.set(i, "" + Double.parseDouble(input.get(i - 1)) * Double.parseDouble(input.get(i + 1)));
         input.remove(i - 1);
         input.remove(i);
-        i -= 1;
+        i--;
       }
       else if (input.get(i).equals("/")){
         input.set(i, "" + Double.parseDouble(input.get(i - 1)) / Double.parseDouble(input.get(i + 1)));
         input.remove(i - 1);
         input.remove(i);
-        i -= 1;
+        i--;
       }
     }
     for (int i = 0; i < input.size(); i++){
@@ -25,15 +28,38 @@ public class Calculator{
         input.set(i, "" + (Double.parseDouble(input.get(i - 1)) + Double.parseDouble(input.get(i + 1))));
         input.remove(i - 1);
         input.remove(i);
-        i -= 1;
+        i--;
       }
       else if (input.get(i).equals("-")){
         input.set(i, "" + (Double.parseDouble(input.get(i - 1)) - Double.parseDouble(input.get(i + 1))));
         input.remove(i - 1);
         input.remove(i);
-        i -= 1;
+        i--;
       }
     }
-    System.out.println(input.get(0));
+
+    return Double.parseDouble(input.get(0));
+  }
+
+  public static void main(String[] args) {
+    String[] temp = args[0].split(" ");
+    ArrayList<String> input = new ArrayList<>();
+    for (String arg: temp){
+      input.add(arg);
+    }
+    for (int i = 0; i < input.size(); i++){
+      if (input.get(i).equals("(")){
+        boolean swap = false;
+        for (int j = 0; j < input.size(); j++){
+          if (input.get(j).equals(")") && swap == false){
+            solve(input.subList(i + 1, j));
+            input.remove(i);
+            input.remove(i + 1);
+            swap = true;
+          }
+        }
+      }
+    }
+    System.out.println(solve(input));
   }
 }

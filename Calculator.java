72a835b2b1572df44db17dcc2682599ce19ea0e0
Mohammad Jawwad <mjawwad@stuy.edu>
2019-01-09 11:02:46 -0500

    Testing mean and median in calc WIP

--- Calculator.java
+++ Calculator.java
@@ -65,6 +65,19 @@ public class Calculator{
     return 0;
   }
 
+  public static double mean(List<String> input){
+    double ans;
+    for (int i = 1; i < input.size(); i++) {
+      ans += (double)(i);
+    }
+    return ((ans / input.size()) - 1);
+  }
+
+  public static int median(List<String> input){
+    int center = (input.size() / 2) + 1;
+    return (int) Integer.parseInt(input.get(center));
+  }
+
   public static void main(String[] args) {
     System.out.println("\033[107m\033[30m");
     ArrayList<String> input = new ArrayList<>();
@@ -74,6 +87,9 @@ public class Calculator{
         input.add(arg);
       }
       System.out.println(solve(input));
+    }
+    if (args.length > 0 && args[0].equals("MEAN")) {
+      System.out.println(mean(input));
     }
   }
 }

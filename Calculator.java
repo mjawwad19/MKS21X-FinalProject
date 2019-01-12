bc9813db39a75282f4357b96a006399fdb7b7afb
Mohammad Jawwad <mjawwad@stuy.edu>
2019-01-12 03:34:16 -0500

    Expanded factor method to apply to all possible operations for each class (and itself), and added a placeholder error message/tutorial. The user can now find the mean/median, subsitute a number for a variable, apply 4 functions (and on Monomials, 3 on polynomials) by inputting the correct arguments in bash

--- Calculator.java
+++ Calculator.java
@@ -101,11 +101,55 @@ public class Calculator{
     return ans.solveQuad();
   }
 
-  public static Polynomial factor(String input){
+
+  private static String drwl(int len){
+    String a = "-";
+    String l = String.join("", Collections.nCopies(len, a));
+    return l;
+  }
+
+  public static Polynomial factor(String input, String method){
     String[] out = input.split("\\)\\(");
     Polynomial a = Polynomial.parsePoly(out[0].substring(1));
     Polynomial b = Polynomial.parsePoly(out[1].substring(0, out[1].length() - 1));
-    return a.multiply(b);
+    if (method.equals("multiply-pp")) {
+      System.out.println("  " + a + "\n*(" + b + ")\n" + drwl(out[0].length()+ 2));
+      return a.multiply(b);
+    }
+    else if (method.equals("subtract-pp")) {
+      System.out.println("  " + a + "\n-(" + b + ")\n" + drwl(out[0].length()+ 2));
+      a.subtract(b);
+      return a;
+    }
+    else if (method.equals("add-pp")) {
+      System.out.println("  " + a + "\n+(" + b + ")\n" + drwl(out[0].length() + 2));
+      a.add(b);
+      return a;
+    }
+    return a;
+  }
+
+  public static Monomial factorM(String input, String method){
+    String[] out = input.split("\\)\\(");
+    Monomial a = Monomial.parseMono(out[0].substring(1));
+    Monomial b = Monomial.parseMono(out[1].substring(0, out[1].length() - 1));
+    if (method.equals("multiply-mm")) {
+      System.out.println("  " + a + "\n*(" + b + ")\n" + drwl(out[0].length()+ 2));
+      return a.multiply(b);
+    }
+    else if (method.equals("divide-mm")) {
+        System.out.println("  " + a + "\n/(" + b + ")\n" + drwl(out[0].length()+ 2));
+        return a.divide(b);
+    }
+    else if (method.equals("subtract-mm")) {
+        System.out.println("  " + a + "\n-(" + b + ")\n" + drwl(out[0].length()+ 2));
+        return a.subtract(b);
+    }
+    else if (method.equals("add-mm")) {
+        System.out.println("  " + a + "\n+(" + b + ")\n" + drwl(out[0].length() + 2));
+        return a.add(b);
+    }
+    else return b;
   }
 
   public static double mean(List<String> input){
@@ -129,37 +173,67 @@ public class Calculator{
   }
 
   public static void main(String[] args) {
-    ArrayList<String> input = new ArrayList<>();
-    if (args.length > 0 && args[0].equals("PEMDAS")){
-      String[] temp = args[1].split(" ");
-      for (String arg: temp){
-        input.add(arg);
+    String msg = "If you would like to use this calculator, please use the following format:  \n\nmean [num1] [num2]...  \nmedian [num1] [num2]... \nsolve-quadratic \" [quadratic polynomial] \" \nadd-pp \" [polynomial])([polynomial] \" \nsubtract-pp \" [polynomial])([polynomial] \" \nmultiply-pp \" [polynomial])([polynomial] \" \nadd-mm \" [monomial])([monomial]  \" \nsubtract-mm \" [monomial])([monomial]  \" \nmultiply-mm \" [monomial])([monomial] ] \" \ndivide-mm \" [monomial])([monomial] \" \nsub-p \" [polynomial] \" [int] \nsub-m \" [monomial] \" [int]";
+    String needTest = "linear \" [monomial] \" \n\ngraph [polynomial] [xbound1] [xbound2] [ybound1] [ybound2] or graph [polynomial]";
+    try {
+      ArrayList<String> input = new ArrayList<>();
+      if (args.length > 0 && args[0].equals("PEMDAS")){
+        String[] temp = args[1].split(" ");
+        for (String arg: temp){
+          input.add(arg);
+        }
+        System.out.println(solve(input));
       }
-      System.out.println(solve(input));
-    }
-    else if (args.length > 0 && args[0].equals("MEAN")) {
-      for (int i = 1; i < args.length; i++){
-        input.add(args[i]);
+      else if (args.length > 0 && args[0].equals("mean")) {
+        for (int i = 1; i < args.length; i++){
+          input.add(args[i]);
+        }
+        System.out.println("Mean: " + mean(input));
       }
-      System.out.println("Mean: " + mean(input));
-    }
-    else if (args.length > 0 && args[0].equals("MEDIAN")) {
-      for (int i = 1; i < args.length; i++) {
-        input.add(args[i]);
+      else if (args.length > 0 && args[0].equals("median")) {
+        for (int i = 1; i < args.length; i++) {
+          input.add(args[i]);
+        }
+        System.out.println("Median: " + median(input));
       }
-      System.out.println("Median: " + median(input));
-    }
-    else if (args.length > 0 && args[0].equals("solve-quadratic")){
-      System.out.println(Arrays.toString(Quad(args[1])));
-    }
-    else if (args.length > 0 && args[0].equals("sub-poly")){
-      System.out.println(Polynomial.parsePoly(args[1]).sub(Integer.parseInt(args[2])));
-    }
-    if (args.length > 0 && args[0].equals("factor")){
-      System.out.println(factor(args[1]));
-    }
-    if (args.length > 0 && args[0].equals("linear")){
-      System.out.println(Polynomial.linear(args[1]));
+      else if (args.length > 0 && args[0].equals("solve-quadratic")){
+        System.out.println("Real Roots Found: " + Arrays.toString(Quad(args[1])));
+      }
+      else if (args.length > 0 && args[0].equals("sub-p")){
+        System.out.println(Polynomial.parsePoly(args[1]).sub(Integer.parseInt(args[2])));
+      }
+      else if (args.length > 0 && args[0].equals("sub-m")){
+        System.out.println(Monomial.parseMono(args[1]).sub(Integer.parseInt(args[2])));
+      }
+      else if (args.length > 0 && (args[0].equals("multiply-pp") ||
+                                   args[0].equals("add-pp") ||
+                                   args[0].equals("subtract-pp"))){
+        System.out.println(factor(args[1],args[0]));
+      }
+      else if (args.length > 0 && (args[0].equals("multiply-mm") ||
+                                   args[0].equals("add-mm") ||
+                                   args[0].equals("subtract-mm") ||
+                                   args[0].equals("divide-mm"))){
+        System.out.println(factorM(args[1],args[0]));
+                                   }
+      /*else if (args.length > 0 && args[0].equals("linear")){
+        System.out.println(Polynomial.linear(args[1]));
+      }
+      else if (args.length > 0 && args[0].equals("graph")){
+        if (!(args.length > 1)) {
+          Graph g1 = new Graph(Polynomial.parsePoly(args[1]));
+          System.out.println(g1);
+        }
+        else if (!(args.length < 6)) {
+          Graph g1 = new Graph(Integer.parseInt(args[2]), Integer.parseInt(args[3]),
+                               Integer.parseInt(args[4]), Integer.parseInt(args[5]), Polynomial.parsePoly(args[1]));
+          System.out.println(g1);
+        }
+      }*/
+      else System.out.println(msg);
+    }catch(Exception e) {
+      System.out.println(msg);
+      //e.printStackTrace();
     }
   }
 }

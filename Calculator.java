854afbe875177953c3fd05c5f1ecebfb161fae39
Mohammad Jawwad <mjawwad@stuy.edu>
2019-01-12 18:58:45 -0500

    Added fxnality /instructions for graph/linear

--- Calculator.java
+++ Calculator.java
@@ -101,15 +101,14 @@ public class Calculator{
     return ans.solveQuad();
   }
 
-
-  private static String drwl(int len){ 
+  private static String drwl(int len){
     String a = "-";
     String l = String.join("", Collections.nCopies(len, a));
     return l;
   }
 
   public static Polynomial factor(String input, String method){
-    String[] out = input.split("\\)\\(");
+    String[] out = input.split("_");
     Polynomial a = Polynomial.parsePoly(out[0].substring(1));
     Polynomial b = Polynomial.parsePoly(out[1].substring(0, out[1].length() - 1));
     if (method.equals("multiply-pp")) {
@@ -130,7 +129,7 @@ public class Calculator{
   }
 
   public static Monomial factorM(String input, String method){
-    String[] out = input.split("\\)\\(");
+    String[] out = input.split("_");
     Monomial a = Monomial.parseMono(out[0].substring(1));
     Monomial b = Monomial.parseMono(out[1].substring(0, out[1].length() - 1));
     if (method.equals("multiply-mm")) {
@@ -173,8 +172,11 @@ public class Calculator{
   }
 
   public static void main(String[] args) {
-    String msg = "If you would like to use this calculator, please use the following format:  \n\nmean [num1] [num2]...  \nmedian [num1] [num2]... \nsolve-quadratic \" [quadratic polynomial] \" \nadd-pp \" [polynomial])([polynomial] \" \nsubtract-pp \" [polynomial])([polynomial] \" \nmultiply-pp \" [polynomial])([polynomial] \" \nadd-mm \" [monomial])([monomial]  \" \nsubtract-mm \" [monomial])([monomial]  \" \nmultiply-mm \" [monomial])([monomial] ] \" \ndivide-mm \" [monomial])([monomial] \" \nsub-p \" [polynomial] \" [int] \nsub-m \" [monomial] \" [int]";
-    String needTest = "linear \" [monomial] \" \n\ngraph [polynomial] [xbound1] [xbound2] [ybound1] [ybound2] or graph [polynomial]";
+    String m1 = "\n\nTo write a polynomial/monomial (nomial for both), please note the following: \n\nVariables to a degree are encapsulated in parenthesis: x^(5)\nPlease place quotation marks with one space from either end around your nomial(s) \" [nomial(s)] \" \nTo have multiply nomials, please place a '_' in between them: \" [nomial]_[nomial] \"";
+    String p = "\n\nsolve-quadratic \" [quadratic polynomial] \" \n\nadd-pp \" [polynomial]_[polynomial] \" \n\nsubtract-pp \" [polynomial]_[polynomial] \" \n\nmultiply-pp \" [polynomial]_[polynomial] \" \n\n";
+    String needTest = "linear \" [polynomial] \"";
+    String m = "add-mm \" [monomial]_[monomial]  \" \n\nsubtract-mm \" [monomial]_[monomial]  \" \n\nmultiply-mm \" [monomial]_[monomial] ] \" \n\ndivide-mm \" [monomial]_[monomial] \"";
+    String msg = m1 + "\n\nIf you would like to use this calculator, please use the following format:  \n\n\nmean [num1] [num2]...  \n\nmedian [num1] [num2]...\n\nsub-p \" [polynomial] \" [int] \n\nsub-m \" [monomial] \" [int]\n\ngraph \" [nomial] \" [xbound1] [xbound2] [ybound1] [ybound2] or graph \" [nomial] \"\n\n" + needTest + p + m;
     try {
       ArrayList<String> input = new ArrayList<>();
       if (args.length > 0 && args[0].equals("PEMDAS")){
@@ -216,20 +218,23 @@ public class Calculator{
                                    args[0].equals("divide-mm"))){
         System.out.println(factorM(args[1],args[0]));
                                    }
-      /*else if (args.length > 0 && args[0].equals("linear")){
+      else if (args.length > 0 && args[0].equals("linear")){
+        //System.out.println(args[1]);
         System.out.println(Polynomial.linear(args[1]));
       }
-      else if (args.length > 0 && args[0].equals("graph")){
-        if (!(args.length > 1)) {
+      else if ((args.length == 2 || args.length == 6) && args[0].equals("graph")){
+        if (args.length == 2) {
+          //System.out.println(args[1]);
           Graph g1 = new Graph(Polynomial.parsePoly(args[1]));
           System.out.println(g1);
         }
-        else if (!(args.length < 6)) {
+        if (args.length == 6) {
           Graph g1 = new Graph(Integer.parseInt(args[2]), Integer.parseInt(args[3]),
                                Integer.parseInt(args[4]), Integer.parseInt(args[5]), Polynomial.parsePoly(args[1]));
           System.out.println(g1);
         }
-      }*/
+        else System.out.println("Too many bounds/Too little, refer to instructions!");
+      }
       else System.out.println(msg);
     }catch(Exception e) {
       System.out.println(msg);
